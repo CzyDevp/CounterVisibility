61ac80ee716eb9c0d04d8e8a4d4065b3844c0929
Nav Singh <nav_singh@manulife.com>
2019-02-08 13:26:32 -0500

    init

---
+++ app/src/main/java/com/example/sinnavj/testinputlayout/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.sinnavj.testinputlayout
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
